a0a9b8b089e86922b31d87f225fe6fb56f41dc94
Peterd03 <pvdickson03@gmail.com>
2026-02-19 20:56:39 -0800

    Refactor affordability data loading and update paths

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from sklearn.decomposition import PCA
 import plotly.express as px
 
 res_path = "data/College_Results.csv"
-aff_path = "data/affordability.csv"
+aff_path = "data/Affordability_Gap.csv"
 
 aff_tbl = [
     "Unit ID", "Student Family Earnings Ceiling", "State Abbreviation", "City", "Sector Name", "Degree of Localization Name",
@@ -365,4 +365,5 @@ def display_output(df, n=20):
     df["similarity_score"] = (df["similarity_score"] * 100).round(1).astype(str) + "%"
     return df[["similarity_score","roi","institution_name","state","city","msi_type","coa_in_state","coa_out_state",
                "total_enrollment","admit_rate","admissions_url"]]
+
 
